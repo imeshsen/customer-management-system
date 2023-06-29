700affe1a1922848e87c629c83306329c2ff4bae
Imesh Senanayake <imeshsenanayake1996@gmail.com>
2023-06-29 16:37:02 +0530

    Modify run.sh

--- run.sh
+++ run.sh
@@ -4,10 +4,6 @@ docker network create spring-network
 
 docker build -t spring .
 
-#docker run -d --name mysql-container --network spring-network -e MYSQL_ALLOW_EMPTY_PASSWORD=true mysql:latest
+docker run -d --name mysql-container --network spring-network -e MYSQL_ALLOW_EMPTY_PASSWORD=true mysql:latest
 
-docker run -d --network spring-network -e MYSQL_ALLOW_EMPTY_PASSWORD=true mysql:latest
-
-#docker run -d --name spring-container --network spring-network -p 8080:8080 -e SPRING_DATASOURCE_URL=jdbc:mysql://mysql-container:3306/customer?createDatabaseIfNotExist=true -e SPRING_DATASOURCE_USERNAME=root spring
-
-docker run -d --network spring-network -p 8081:8081 -e SPRING_DATASOURCE_URL=jdbc:mysql://mysql-container:3306/customer?createDatabaseIfNotExist=true -e SPRING_DATASOURCE_USERNAME=root spring
+docker run -d --name spring-container --network spring-network -p 8081:8081 -e SPRING_DATASOURCE_URL=jdbc:mysql://mysql-container:3306/customer?createDatabaseIfNotExist=true -e SPRING_DATASOURCE_USERNAME=root spring
